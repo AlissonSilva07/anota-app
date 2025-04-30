8027f191d7931a82ee5a59cbdde708a91db7e5ef
Alisson Rego <alisson.rego@icc.org.br>
2025-04-30 17:00:11 -0300

    feat(home): create fab

--- app/src/main/java/edu/alisson/anota/presentation/navigation/MainScaffold.kt
+++ app/src/main/java/edu/alisson/anota/presentation/navigation/MainScaffold.kt
@@ -1,16 +1,25 @@
 package edu.alisson.anota.presentation.navigation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import edu.alisson.anota.presentation.components.BottomNavBar
 import java.util.Locale
@@ -24,6 +33,9 @@ fun MainScaffold(
 ) {
     val currentDestination = navController.currentDestination?.route
     val currentDestinationName = currentDestination?.substringAfterLast("/")?.capitalize(Locale.getDefault()).toString()
+    val fabRoutes = listOf(
+        Screen.Home.route
+    )
 
     Scaffold(
         topBar = {
@@ -37,6 +49,31 @@ fun MainScaffold(
                         )
                     }
                 )
+            } else null
+        },
+        floatingActionButton = {
+            if (currentDestination in fabRoutes) {
+                FloatingActionButton(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(8.dp),
+                    onClick = {}
+                ) {
+                    Row(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Add,
+                            contentDescription = "Nova Nota"
+                        )
+                        Text(
+                            text = "Nova Nota",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                }
             } else null
         },
         bottomBar = {
